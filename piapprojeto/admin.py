6c105f0f8b74f27265f83658b475cefcf81ade37
VitXy <vitor.otavio.munaretto@gmail.com>
2025-11-27 19:44:58 -0300

    Atualizando branch vitor

--- piapprojeto/admin.py
+++ piapprojeto/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Tarefa, PerfilUsuario
 
-# Register your models here.
+
+@admin.register(Tarefa)
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "usuario", "concluida", "data_criacao", "data_conclusao")
+    list_filter = ("concluida", "data_criacao")
+    search_fields = ("titulo", "descricao")
+    ordering = ("-data_criacao",)
+
+    fieldsets = (
+        ("Informações da Tarefa", {
+            "fields": ("usuario", "titulo", "descricao")
+        }),
+        ("Status", {
+            "fields": ("concluida",),
+        }),
+    )
+
+
+@admin.register(PerfilUsuario)
+class PerfilUsuarioAdmin(admin.ModelAdmin):
+    list_display = ("usuario", "xp", "dias_consecutivos", "ultima_conclusao")
+    search_fields = ("usuario__username",)
+    ordering = ("-xp",)
